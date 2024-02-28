3c40655120dd5f33e8f9430ed7376a420f2f1ec7
leedy-dev <ldy033000@gmail.com>
2024-02-28 15:54:19 +0900

    eslint, prettier 수정 (airbnb)

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import './App.css';
-import React from "react";
+import React from 'react';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-        Hello,
-        This is React With Typescript.
-    </div>
-  );
-}
+  return <div className="App">Hello, This is React With Typescript.</div>;
+};
 
 export default App;
