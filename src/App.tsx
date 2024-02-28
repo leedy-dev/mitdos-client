fd2b248868bd88cc1d0331772f5c7faac1fda20e
leedy-dev <ldy033000@gmail.com>
2024-02-28 14:39:48 +0900

    typescript 및 eslint, prettier 적용

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+        Hello,
+        This is React With Typescript.
+    </div>
+  );
+}
+
+export default App;
